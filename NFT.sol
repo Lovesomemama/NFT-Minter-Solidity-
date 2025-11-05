7a83911c10685e4139481618d305ab2322c57889
Pavel Melnychuk <51413815+Lovesomemama@users.noreply.github.com>
2025-11-05 16:55:08 +0100

    Refactor: Update NFT name and symbol for testing

--- NFT.sol
+++ NFT.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract MyNFT is ERC721 {
     uint256 public tokenId;
-    constructor() ERC721("MyNFT", "MNFT") {}
+    constructor() ERC721("TestNFT", "TNFT") {}
 
 // Mint a new NFT to the caller and increment ID
     function mint() public {
